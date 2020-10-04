e4636648bc7f3963c37d0a0c5a668676dcc8c59c
kukusiik <polinaosina14@gmail.com>
2020-10-04 16:34:16 +0300

    add initial prj

---
+++ WidgetsSample/WidgetsSample/WidgetsSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetsSampleApp.swift
+//  WidgetsSample
+//
+//  Created by Полина Осина on 04.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct WidgetsSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
